0af513f85723b13ff09a056349f217a2dbc6881d
Danny C PC <dannyc52521@gmail.com>
2021-07-19 15:32:51 +1000

    contactUs module added

---
+++ src/contact.js
@@ -0,0 +1,10 @@
+import { loadPage } from "./loadPage";
+
+const contendDiv = document.querySelector(".content");
+
+const contactUs = () => {
+  const contactUs = document.createElement("main");
+    contactUs.innerHTML = `
+    
+    `;
+}
